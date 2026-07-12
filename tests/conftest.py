19693091b10d6408d68ee7031abf783f3abfb8ce
Atharva Wadekar <theteckky@gmail.com>
2026-07-12 13:09:11 +0530

    test: add global test_app_tests.py to run all backend subtests from root tests folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,4 @@
-import pytest
-
-@pytest.fixture(scope='session')
-def django_db_setup(django_db_blocker):
-    """
-    Custom setup for Django database tests.
-    We can unblock the database block here to run migrations or seed data if needed.
-    """
-    with django_db_blocker.unblock():
-        pass
+# Register the backend conftest as a plugin to load its fixtures and factories.
+pytest_plugins = [
+    "backend.conftest",
+]
